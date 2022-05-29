7d45b1a3a9416a71a796978aad7ab04cf528f3f7
Gwonsiyun <49703174+Gwonsiyun@users.noreply.github.com>
2022-05-29 17:01:21 +0900

    Add files via upload

---
+++ src/main/java/com/gwonsiyun/home_friends/HomeFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.gwonsiyun.home_friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwonsiyun/home_friends/HomeFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwonsiyun.home_friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
